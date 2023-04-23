4222c7f370c3e53942ae5d2aa0709901794899fb
Ekansh Gupta <103844989+ekanshgupta981@users.noreply.github.com>
2023-04-23 16:42:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const apiRoutes = require("./app/routes/api-routes");
 const app = express();
+const PORT = 4000;
 
 app.use(cors());
 app.use(express.json());
@@ -17,7 +18,7 @@ mongoose
   .connect(process.env.MONGODB_URI)
   .then(
     () =>
-      app.listen(process.env.PORT, function () {
+      app.listen(process.env.PORT || 4000, function () {
         console.log(
           "your server is running on http://localhost:",
           process.env.PORT
